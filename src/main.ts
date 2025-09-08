da41bf4512e5688a2522dfc1d87a9b334ca3bd2f
Lebi <lebihannathan@gmail.com>
2025-09-08 22:31:18 +0200

    https for local

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+// In main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const httpsOptions = {
+    key: fs.readFileSync('./certificates/localhost+1-key.pem'),
+    cert: fs.readFileSync('./certificates/localhost+1.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  await app.listen(3000);
 }
 bootstrap();
